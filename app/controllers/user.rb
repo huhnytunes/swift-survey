f1cbfeaf7024ab9205762673af15ea92f40db0da
Simon Says... push to master & Joe <pair+gitnasty.jdcohen9@devbootcamp.com>
2014-06-13 16:48:12 -0400

    reconcile

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,11 +1,11 @@
 ## USER(SURVEY CREATOR) PAGES ################################
-get '/users/surveys'
+get '/users/surveys' do
   @surveys = Survey.where(creator_id: current_user)
   erb :created_surveys
 end
 
 
-get '/users/surveys/:survey_id'
+get '/users/surveys/:survey_id' do
   @survey = Survey.where(survey_id: params[:survey_id])
   erb :survey_stats
 end
