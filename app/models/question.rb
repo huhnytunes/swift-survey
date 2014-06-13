0478eba2e1c28a2da70939b619e1406eef0d8831
Judy Jow <judy.jow@gmail.com>
2014-06-13 03:10:26 -0400

    Add taking survey functionality and other minor changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,16 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :choices
   has_many :responses, through: :choices
+
+  def as_json(options={})
+  	{id: self.id, content: self.content, choices: self.choices}
+  end
   
+  def choice_percents 
+  	response_count = self.responses.length 
+  	self.choices.map do |choice|
+  		{id: choice.id, percent: (1.0 * choice.responses.length / response_count), content: choice.content}
+  	end
+  end
+
 end
