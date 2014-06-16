a449e0cf474c0fd4724f6c95e0109c6f9ebc8e48
Simon Says... push to master & Judy Jow <pair+gitnasty.jjow@devbootcamp.com>
2014-06-16 08:42:46 -0400

    implement majority of create survey function mvc js

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -3,13 +3,12 @@ get '/surveys/new' do
 end
 
 post '/surveys/create' do
-  survey = Survey.create(title: params[:title], creator_id: current_user)
-  question = Question.create(content: params[:question], survey_id: survey.id)
-  params[:choice].each do |choice|
-    choice_index = 1
-    Choice.create(content: params[:choice][choice_index],question_id: question.id)
-    choice_index += 1
-  end
+  p params
+  # survey = Survey.create(title: params[:title], creator_id: current_user)
+  # question = Question.create(content: params[:question], survey_id: survey.id)
+  # params[:choice].each do |choice|
+  #   Choice.create(content: params[:choice], question_id: question.id)
+  # end
 
   redirect '/users/surveys'
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,15 @@
-/*//////////////////////////////////////////////// 
-DOCUMENT READY 
+/*////////////////////////////////////////////////
+DOCUMENT READY
 */////////////////////////////////////////////////
 
 $(document).ready(function() {
 	var survey = new Survey();
 	var view = new View(survey);
 	var controller = new Controller($('body'),survey, view)
-
 });
 
-/*//////////////////////////////////////////////// 
-CONTROLLER 
+/*////////////////////////////////////////////////
+CONTROLLER
 */////////////////////////////////////////////////
 
 function Controller(el, survey, view) {
@@ -18,17 +17,19 @@ function Controller(el, survey, view) {
 	this.survey = survey;
 	this.view = view;
 
+	var self = this;
+
 	// CREATE SURVEY EVENTS
-	// listen for button to create new survey
-	$('.create_survey').on('click', this.createSurvey.bind(this));
+	// // listen for button to create new survey
+	// $('.create_survey').on('click', this.createSurvey.bind(this));
 	// listen for button to add new question
 	$('.new_question').on('click', this.addNewQuestion.bind(this));
 	// listen for button to submit survey
 	$('.submit_survey').on('click', this.submitSurvey.bind(this));
 	// listen for button to add choice to current question
-	$('.add_choice').on('click', this.addChoice.bind(this));
+	$('#question').on('click', '.add_choice', this.addChoice.bind(this));
 	// listen for button to remove choice from current question
-	$('.remove_choice').on('click', this.removeChoice.bind(this));
+	$('#question').on('click', '.remove_class', this.removeChoice.bind(this));
 
 	// TAKE SURVEY EVENTS
 	// listen for button to take new survey
@@ -48,13 +49,15 @@ Controller.prototype.createSurvey = function(e) {
 };
 
 Controller.prototype.addNewQuestion = function(e) {
+	console.log(e.target)
 	// based on the user input, add a new question object to this.survey questions array
-	this.view.displayAddNewQuestion(); // ask view to display a new blank form 
+	this.view.displayAddNewQuestion(e.target); // ask view to display a new blank form
+
 };
 
 Controller.prototype.submitSurvey = function(e) {
-	this.survey.submitSurvey(); // have survey object send ajax request to save the survey (and its questions/choices) to the database 
-	// dont use e.preventDefault(). in the erb, have the route redirect to user's homepage. 
+	this.survey.submitSurvey(); // have survey object send ajax request to save the survey (and its questions/choices) to the database
+	// dont use e.preventDefault(). in the erb, have the route redirect to user's homepage.
 	// this way, we both have an ajax request and a route to the users homepage
 };
 
@@ -66,23 +69,34 @@ Controller.prototype.getSurvey = function(e) {
 
 Controller.prototype.getNextQuestion = function(e) {
 	// update appropriate question.userChoice based on what the user selected
-	this.view.displayGetNextQuestion(); // ask the view to display the next question: 
+	this.view.displayGetNextQuestion(); // ask the view to display the next question:
 };
 
 Controller.prototype.finishSurvey = function(e) {
 	// when this happens, user has answered every question in the survey (i.e. userChoice property exists for every question in the survey)
 	// we need to save this info in the db via AJAX => ask survey to do this
-	this.survey.finishSurvey(); 
+	this.survey.finishSurvey();
 	// dont use e.preventDefault(). in erb, the route should redirect to some page (thanks for taking survey, etc?)
 	// this way, we both have an ajax request and a route to the users homepage
 };
 
-/*//////////////////////////////////////////////// 
-MODELS 
+Controller.prototype.addChoice = function(e) {
+	// console.log(e.target)
+	this.view.displayAddedChoice(e.target);
+}
+
+Controller.prototype.removeChoice = function(e) {
+	console.log("anything")
+	console.log(e.target)
+	this.view.removeAddedChoice(e.target)
+}
+
+/*////////////////////////////////////////////////
+MODELS
 */////////////////////////////////////////////////
 
 function Survey() {
-	this.id; 
+	this.id;
 	this.title;
 	this.questions = [];
 	this.currentQuestionIndex = 0 // starts at 0. increment/decrement based on user clicks (next/previous question). you will know which question to grab from the questions array
@@ -116,7 +130,7 @@ Survey.prototype.addNewQuestion = function() {
 };
 
 Survey.prototype.submitSurvey = function() {
-	
+
 };
 
 function Question() {
@@ -124,15 +138,17 @@ function Question() {
 	this.answered = false;
 	this.content;
 	this.choices = {}; // an object where property is choice.id and value is choice.content
-	this.userChoice; // the choice.id that the user selected. this will be used when we need to save a user's response. 
+	this.userChoice; // the choice.id that the user selected. this will be used when we need to save a user's response.
 };
 
-/*//////////////////////////////////////////////// 
-VIEWS 
+/*////////////////////////////////////////////////
+VIEWS
 */////////////////////////////////////////////////
 
 function View(survey) {
 	this.survey = survey;
+	this.questionNumber = 1;
+	this.choiceNumber = 3;
 };
 
 View.prototype.displayGetNextQuestion = function() {
@@ -142,6 +158,25 @@ View.prototype.displayGetNextQuestion = function() {
 };
 
 View.prototype.displayAddNewQuestion = function() {
-	// display a form for user to add a new question
+	this.choiceNumber = 3;
+	$("ul").append(
+		'<p>Question: </p><input type="text" name="' + this.questionNumber++ +
+		'[]"> <p>Choices: </p> <li><input type="text" name="' + this.questionNumber +
+		'[][1]"></li> <li><input type="text" name="' + this.questionNumber +
+		'[][2]"></li> <button class="add_choice" type="button">Add Choice</button><br>')
 };
 
+View.prototype.displayAddedChoice = function(e) {
+	// this.questionNumber =
+	// var currentQuestionNumber =
+	console.log(this);
+	console.log($(e).next())
+	$(e).prev().after("<li><input type='text' name=" +
+		this.questionNumber + "[][" + this.choiceNumber++ +
+		"]> <button class='remove_class' type='button'>delete</button><br></li>")
+}
+
+View.prototype.removeAddedChoice = function(e) {
+	console.log(e)
+	$(e).parent().remove();
+}
