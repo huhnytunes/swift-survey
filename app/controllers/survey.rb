2a34f15373edf7e740a1601554f8c87b1d278597
Simon Says... push to master & Judy Jow <pair+gitnasty.jjow@devbootcamp.com>
2014-06-16 10:06:59 -0400

    close the loop--can parse create survey form data

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -4,11 +4,17 @@ end
 
 post '/surveys/create' do
   p params
-  # survey = Survey.create(title: params[:title], creator_id: current_user)
-  # question = Question.create(content: params[:question], survey_id: survey.id)
-  # params[:choice].each do |choice|
-  #   Choice.create(content: params[:choice], question_id: question.id)
-  # end
+  num_of_questions = params.length - 1
+  question_number = 1
+  p params["1"][1]
+  survey = Survey.create(title: params[:title], creator_id: current_user.id)
+  num_of_questions.times do
+    question = Question.create(content: params[question_number.to_s][0], survey_id: survey.id)
+    params[question_number.to_s][1].each do |index, content|
+      Choice.create(content: content, question_id: question.id)
+    end
+    question_number += 1
+  end
 
   redirect '/users/surveys'
 end
